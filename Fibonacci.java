d76f553c5a5d9d4509dfcb57f82f27e0eec782f6
antoniovar <61683213+antoniovar@users.noreply.github.com>
2020-05-05 18:39:25 +0200

    Entrega Fibonacci 2
    Entrega de la tarea Fibonacci actualizada y con algunos errores corregidos

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,21 +1,18 @@
 package org.factorialYfibonacci;
 
 public class Fibonacci {
-    int nBBactual=0;
-    int nBBmesPasado=0;
-    int nAdulto=0;
-
     public int compute(int n, int k){ //siendo n número de meses y k el número de parejas de hijos que tiene cada pareja.
         int totalConejos;
-        if (n < 1) {
+        if (n < 1 || k<1 ) {
             throw new RuntimeException("Número de meses no valido:" + n);
         }
-        else if (n == 1){
-            nBBactual = 1 ;
-            nAdulto = 0;
-            return totalConejos = nBBactual + nAdulto;
+        else if (n == 1 || n==2){
+            return 1;
         }
         else{
+            int nBBactual=0;
+            int nBBmesPasado=0;
+            int nAdulto=0;
             nBBmesPasado=1;
             for(int i=1 ; i<n ; i++){
                 nBBactual=nAdulto*3;
